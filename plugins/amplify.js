4bcae8a46c5b1bc1be718636c69e2a6b8bdb325c
pewebdevelopment <76696556+pewebdevelopment@users.noreply.github.com>
2022-10-03 10:43:06 +0530

    configurgin amplify plugin

--- plugins/amplify.js
+++ plugins/amplify.js
@@ -1,2 +1,9 @@
 import Vue from "Vue";
 import Amplify, * as AmplifyModules from 'aws-amplify';
+// import { AmplifyPlugin } from 'aws-amplify-vue';
+
+
+// import Amplify from 'aws-amplify'
+import awsconfig from '~/aws-exports'
+
+Amplify.configure(awsconfig)
